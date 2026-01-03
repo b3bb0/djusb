5d9c691beff7d5160839e2a0c3a71d32bc9f9721
Alberto bebbo Capponi <b3bb0@users.noreply.github.com>
2026-01-03 19:15:28 +0800

    Set base path for GitHub Pages to '/beta/'

--- .github/templates/coming-soon/vite.config.js
+++ .github/templates/coming-soon/vite.config.js
@@ -3,6 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  // For GitHub Pages project sites use "/<repo>/"
-  base: process.env.BASE_PATH ?? "/",
+  base: "/beta/"
 });
